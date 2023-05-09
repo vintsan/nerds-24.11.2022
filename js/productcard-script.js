34c71484802de5a2ad4fecb97dc678e8259f804b
vintsan <vintsan.ya@yandex.ru>
2023-05-09 18:52:04 +0300

    все сделал, конец

---
+++ js/productcard-script.js
@@ -0,0 +1,32 @@
+/* рабочий код для окна .templates-popup на карточках */
+
+
+
+
+/* var productcardlinks = document.querySelectorAll(".templates-img-link");
+productcardlinks.forEach(function (productcardlink) {
+  productcardlink.addEventListener("mouseenter", function (evt) {
+    evt.preventDefault();
+    var productcardpopup = this.nextElementSibling;
+    productcardpopup.classList.add("productcard-modal-show");
+  });
+
+  productcardlink.addEventListener("mouseleave", function (evt) {
+    evt.preventDefault();
+    var productcardpopup = this.nextElementSibling;
+    productcardpopup.classList.remove("productcard-modal-show");
+  });
+});
+
+var productcardpopups = document.querySelectorAll(".templates-popup");
+productcardpopups.forEach(function (productcardpopup) {
+  productcardpopup.addEventListener("mouseenter", function (evt) {
+    evt.preventDefault();
+    this.classList.add("productcard-modal-show");
+  });
+
+  productcardpopup.addEventListener("mouseleave", function (evt) {
+    evt.preventDefault();
+    this.classList.remove("productcard-modal-show");
+  });
+}); */
